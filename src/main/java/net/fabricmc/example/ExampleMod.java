1d0568e9276e2d05727e95e18427ddc867a57652
zchan1325 <3dollee@gmail.com>
2022-05-26 15:01:22 +0900

    hello for fisrt commitment

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -15,7 +15,8 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		
 		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("This is my messege.");
 	}
 }
